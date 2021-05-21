78539460da9e4d4ecfbdb959cb07baaab6e14d0b
gerhard.molin <gmolin@student.hive.fi>
2021-05-21 15:42:09 +0300

    adding triangle solution

--- go/triangle/triangle.go
+++ go/triangle/triangle.go
@@ -1,27 +1,81 @@
-/*
-
-
-*/
 package triangle
 
+import (
+	"math"
+)
 
-// Notice KindFromSides() returns this type. Pick a suitable data type.
-type Kind
+type Kind int
 
 const (
-    // Pick values for the following identifiers used by the test program.
-    NaT // not a triangle
-    Equ // equilateral
-    Iso // isosceles
-    Sca // scalene
+    NaT Kind = iota // 0 = not a triangle
+    Equ // 1 = equilateral
+    Iso // 2 = isosceles
+    Sca // 3 = scalene
 )
 
 // KindFromSides should have a comment documenting it.
 func KindFromSides(a, b, c float64) Kind {
-	// Write some code here to pass the test suite.
-	// Then remove all the stock comments.
-	// They're here to help you get started but they only clutter a finished solution.
-	// If you leave them in, reviewers may protest!
-	var k Kind
+	var k Kind;
+	// First I am checking if the numbers are valid numbers (e.g. inifinte number or just invalid)
+	if (!checkValidNumber(a, b, c)) {
+		k = NaT;
+		return k;
+	}
+	// Then I check for Triangle Equality
+	if (!checkTriangleInequality(a, b, c)) {
+		k = NaT;
+		return k;
+	}
+	// Finally I asses the type of triangle
+	k = assesTriangle(a, b, c, k);
 	return k
+}
+
+
+func checkValidNumber(a, b, c float64) bool {
+	if (math.IsNaN(a) || math.IsNaN(b) || math.IsNaN(c) ||
+		math.IsInf(a, 1) || math.IsInf(b, 1) || math.IsInf(c, 1)) {
+		return false;
+	}
+	return true;
+} 
+
+func checkTriangleInequality(a, b, c float64) bool {
+	if (a <= 0 || b <= 0 || c <= 0) {
+		return false;
+	}
+	if ((a + b) < c || (b + c) < a || (a + c) < b) {
+		return false;
+	}
+	return true;
+} 
+
+func assesTriangle(a, b, c float64, k Kind) Kind {
+	var array [3]float64;
+	array[0] = a;
+	array[1] = b;
+	array[2] = c;
+	countEqu := 0;
+	countIso := 0;
+	for i := 0; i < len(array); i++ {
+		same := 0;
+		for z := 0; z < len(array); z++ {
+			if (array[i] == array[z] && i != z) {
+				same++;
+			}
+		}
+		if (same == 2) {
+			countEqu++;
+		} else if (same == 1) {
+			countIso++;
+		}
+	}
+	if (countEqu == 3) {
+		k = Equ;
+	} else if (countIso == 2) {
+		k = Iso;
+	} else {
+		k = Sca;
+	}
+	return k;
 }
