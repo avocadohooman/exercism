8d6ea00675db1483ae1b400408f30c6f9fad5b61
gerhard.molin <gmolin@student.hive.fi>
2021-06-08 11:11:39 +0300

    adding scrabble solution

--- go/scrabble-score/scrabble_score.go
+++ go/scrabble-score/scrabble_score.go
@@ -13,7 +13,7 @@ Q, Z                               10
 */
 
 // ScoreSchema data type
-type ScoreSchema map[string]int
+type ScoreSchema map[rune]int
 
 // Score function
 func Score(word string) int {
@@ -22,30 +22,17 @@ func Score(word string) int {
 	}
 	totalScore := 0
 	wordLowerCase := strings.ToLower(word)
-	var scoreMap = ScoreSchema{
-		"aeioulnrst": 1,
-		"dg":         2,
-		"bcmp":       3,
-		"fhvmy":      4,
-		"k":          5,
-		"jx":         8,
-		"qz":         10,
+	var scoreMap = map[rune]int{
+		'a': 1, 'e': 1, 'i': 1, 'o': 1, 'u': 1, 'l': 1, 'n': 1, 'r': 1, 's': 1, 't': 1,
+		'd': 2, 'g': 2,
+		'b': 3, 'c': 3, 'm': 3, 'p': 3,
+		'f': 4, 'h': 4, 'v': 4, 'w': 4, 'y': 4,
+		'k': 5,
+		'j': 8, 'x': 8,
+		'q': 10, 'z': 10,
 	}
-	for scoreKey, scoreValue := range scoreMap {
-		totalScore += pointsCalculator(wordLowerCase, scoreKey, scoreValue)
-	}
-	return totalScore
-}
-
-// Point calculator algorithm
-func pointsCalculator(word string, scoreBoard string, scoreValue int) int {
-	totalScore := 0
-	for i := 0; i < len(word); i++ {
-		for z := 0; z < len(scoreBoard); z++ {
-			if word[i] == scoreBoard[z] {
-				totalScore += scoreValue
-			}
-		}
+	for _, letter := range wordLowerCase {
+		totalScore += scoreMap[letter]
 	}
 	return totalScore
 }
