3baee23c6c88015f1a4d6e9f2c0c2cf9c727c69a
gerhard.molin <gmolin@student.hive.fi>
2021-06-10 10:06:34 +0300

    adding better documentaiton for scrabble.go

--- go/scrabble-score/scrabble_score.go
+++ go/scrabble-score/scrabble_score.go
@@ -1,36 +1,25 @@
+// Package scrabble provides a score for a given word.
 package scrabble
 
 import "strings"
 
-/*
-A, E, I, O, U, L, N, R, S, T       1
-D, G                               2
-B, C, M, P                         3
-F, H, V, W, Y                      4
-K                                  5
-J, X                               8
-Q, Z                               10
-*/
+// scoreMap declares the map for the scoring board, used to determine
+// the score for each letter.
+var scoreMap = map[rune]int{
+	'a': 1, 'e': 1, 'i': 1, 'o': 1, 'u': 1, 'l': 1, 'n': 1, 'r': 1, 's': 1, 't': 1,
+	'd': 2, 'g': 2,
+	'b': 3, 'c': 3, 'm': 3, 'p': 3,
+	'f': 4, 'h': 4, 'v': 4, 'w': 4, 'y': 4,
+	'k': 5,
+	'j': 8, 'x': 8,
+	'q': 10, 'z': 10,
+}
 
-// ScoreSchema data type
-type ScoreSchema map[rune]int
-
-// Score function
+// Score takes in a word as a string, calculates the points for this word based on
+// the scoreMap and returns the totalScore as int
 func Score(word string) int {
-	if len(word) <= 0 {
-		return 0
-	}
 	totalScore := 0
 	wordLowerCase := strings.ToLower(word)
-	var scoreMap = map[rune]int{
-		'a': 1, 'e': 1, 'i': 1, 'o': 1, 'u': 1, 'l': 1, 'n': 1, 'r': 1, 's': 1, 't': 1,
-		'd': 2, 'g': 2,
-		'b': 3, 'c': 3, 'm': 3, 'p': 3,
-		'f': 4, 'h': 4, 'v': 4, 'w': 4, 'y': 4,
-		'k': 5,
-		'j': 8, 'x': 8,
-		'q': 10, 'z': 10,
-	}
 	for _, letter := range wordLowerCase {
 		totalScore += scoreMap[letter]
 	}
